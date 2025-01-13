acddb1c651a33765cc55f4d6af08df70360b5eaa
alirezamohammadpoor <alirezamohammadp@gmail.com>
2025-01-13 15:42:59 +0100

    Coded getComputerChoice so it returns rock, paper or scissors randomly.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello, World!");
+// Function that creates a value between 0 1 2.
+function getComputerChoice(max){
+  return Math.floor(Math.random() * max);
+}
+
+const choice = getComputerChoice(3); // stores value from getComputerChoice
+
+// Prints ROCK, PAPER OR SCISSORS instead of 0,1,2.
+if (choice === 0) {
+  console.log("ROCK");
+} else if (choice === 1) {
+  console.log("PAPER");
+} else if (choice === 2) {
+  console.log("SCISSORS");
+}
