381d292fc4af0614e4ba06a25afd013b9a7dbb8a
alirezamohammadpoor <alirezamohammadp@gmail.com>
2025-01-14 12:56:26 +0100

    Coded playGame function that contains 5 rounds. After 5 rounds console announces who won majority of the rounds, player or computer.

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
-// Function that creates a value between 0 1 2.
-function getComputerChoice(max){
-  return Math.floor(Math.random() * max);
-}
+let humanScore = 0;
+let computerScore= 0;
 
-const choice = getComputerChoice(3); // stores value from getComputerChoice
+function playGame() {
+for (let round = 0; round < 5; round++) {
 
-// Prints ROCK, PAPER OR SCISSORS instead of 0,1,2.
-if (choice === 0) {
-  console.log("ROCK");
-} else if (choice === 1) {
-  console.log("PAPER");
-} else if (choice === 2) {
-  console.log("SCISSORS");
-}
-
+// Prompts the user to choose rock, paper or scissors.
 let humanChoice = prompt("Rock, paper or scissors?");
 
 if (humanChoice == "rock") {
@@ -22,4 +13,49 @@ if (humanChoice == "rock") {
   console.log("PAPER");
 } else if (humanChoice == "scissors") {
   console.log("SCISSORS");
+}
+
+// Function that creates a value between 0 1 2.
+function getComputerChoice(max){
+  return Math.floor(Math.random() * max);
+}
+
+const computerChoice = getComputerChoice(3); // stores value from getComputerChoice
+
+// Prints ROCK, PAPER OR SCISSORS instead of 0,1,2.
+if (computerChoice === 0) {
+  console.log("ROCK");
+} else if (computerChoice === 1) {
+  console.log("PAPER");
+} else if (computerChoice === 2) {
+  console.log("SCISSORS");
+}
+
+if (
+  (humanChoice === "rock" && computerChoice === 0) ||
+  (humanChoice === "paper" && computerChoice === 1) ||
+  (humanChoice === "scissors" && computerChoice === 2)
+) {
+  console.log("It is a tie!");
+} else if (
+  (humanChoice === "rock" && computerChoice === 2) ||
+  (humanChoice === "paper" && computerChoice === 0) ||
+  (humanChoice === "scissors" && computerChoice === 1)
+) {
+  console.log("Player won!");
+humanScore++;
+} else {
+  console.log("Computer won!");
+  computerScore++;
+}
+
+
+}
+}
+playGame()
+
+if (humanScore > computerScore) {
+  console.log("PLAYER IS THE CHAMPION!");
+} else {
+  console.log("COMPUTER IS THE CHAMPION!");
 }
