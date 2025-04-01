116071c0ca879347323c1469a9f4796e4905b88f
alirezamohammadpoor <alirezamohammadp@gmail.com>
2025-04-01 12:29:33 +0200

    Added UI to RPS game

---
+++ loop.js
@@ -0,0 +1,5 @@
+// for (let i = 100; i < 100; input++) {
+let i = 100;
+let input = prompt(`Enter a number above ${i}`);
+
+if (input < 100 input++)
--- script.js
+++ script.js
@@ -1,61 +1,133 @@
 let humanScore = 0;
-let computerScore= 0;
+let computerScore = 0;
+let roundCount = 0;
+let humanChoice;
+const announceChampion = document.querySelector(".champion");
 
-function playGame() {
-for (let round = 0; round < 5; round++) {
+function gameOver() {
+  if (roundCount === 5) {
+    const pickContainer = document.querySelector(".pick-container");
+    pickContainer.textContent = "";
 
-// Prompts the user to choose rock, paper or scissors.
-let humanChoice = prompt("Rock, paper or scissors?");
+    const buttonsContainer = document.querySelector(".buttons");
+    buttonsContainer.style.display = "none";
 
-if (humanChoice == "rock") {
-  console.log("ROCK");
-} else if (humanChoice == "paper") {
-  console.log("PAPER");
-} else if (humanChoice == "scissors") {
-  console.log("SCISSORS");
+    const gameTitle = document.querySelector("h1");
+    gameTitle.style.display = "none";
+
+    if (humanScore > computerScore) {
+      const humanChampion = document.createElement("h1");
+      humanChampion.classList.add("human-champion");
+      humanChampion.textContent = "HUMAN IS THE CHAMPION";
+      announceChampion.appendChild(humanChampion);
+    } else if (humanScore < computerScore) {
+      const computerChampion = document.createElement("h1");
+      computerChampion.classList.add("computer-champion");
+      computerChampion.textContent = "COMPUTER IS THE CHAMPION";
+      announceChampion.appendChild(computerChampion);
+    } else {
+      const tieChampion = document.createElement("h1");
+      tieChampion.classList.add("tie-champion");
+      tieChampion.textContent = "IT'S A TIE!";
+      announceChampion.appendChild(tieChampion);
+    }
+  }
 }
 
 // Function that creates a value between 0 1 2.
-function getComputerChoice(max){
+function getComputerChoice(max) {
   return Math.floor(Math.random() * max);
 }
 
-const computerChoice = getComputerChoice(3); // stores value from getComputerChoice
+function playRound() {
+  const computerChoice = getComputerChoice(3);
 
-// Prints ROCK, PAPER OR SCISSORS instead of 0,1,2.
-if (computerChoice === 0) {
-  console.log("ROCK");
-} else if (computerChoice === 1) {
-  console.log("PAPER");
-} else if (computerChoice === 2) {
-  console.log("SCISSORS");
+  // Print player
+  const pickContainer = document.querySelector(".pick-container");
+  pickContainer.textContent = "";
+
+  const humanPick = document.createElement("p");
+  humanPick.classList.add("human-pick");
+  humanPick.textContent = `Player chose: ${humanChoice}`;
+
+  // Convert computerChoice to string and print it out
+  let computerChoiceText;
+  if (computerChoice === 0) {
+    computerChoiceText = "ROCK";
+  } else if (computerChoice === 1) {
+    computerChoiceText = "PAPER";
+  } else if (computerChoice === 2) {
+    computerChoiceText = "SCISSORS";
+  }
+
+  const computerPick = document.createElement("p");
+  computerPick.classList.add("computer-pick");
+  computerPick.textContent = `Computer chose: ${computerChoiceText}`;
+
+  pickContainer.appendChild(humanPick);
+  pickContainer.appendChild(computerPick);
+
+  // Determine winner
+  if (
+    (humanChoice === "ROCK" && computerChoice === 0) ||
+    (humanChoice === "PAPER" && computerChoice === 1) ||
+    (humanChoice === "SCISSORS" && computerChoice === 2)
+  ) {
+    const tieResult = document.createElement("p");
+    tieResult.classList.add("tie-result");
+    tieResult.textContent = "It's a tie!";
+
+    pickContainer.appendChild(tieResult);
+  } else if (
+    (humanChoice === "ROCK" && computerChoice === 2) ||
+    (humanChoice === "PAPER" && computerChoice === 0) ||
+    (humanChoice === "SCISSORS" && computerChoice === 1)
+  ) {
+    const humanWinner = document.createElement("p");
+    humanWinner.classList.add("human-winner");
+    humanWinner.textContent = "Human won!";
+
+    pickContainer.appendChild(humanWinner);
+
+    humanScore++;
+  } else {
+    const computerWinner = document.createElement("p");
+    computerWinner.classList.add("computer-winner");
+    computerWinner.textContent = "Computer won!";
+
+    pickContainer.appendChild(computerWinner);
+    computerScore++;
+  }
+
+  // Print current score
+  const currentHumanScore = document.querySelector("#humanScore");
+  const currentComputerScore = document.querySelector("#computerScore");
+
+  currentHumanScore.textContent = humanScore;
+  currentComputerScore.textContent = computerScore;
 }
 
-if (
-  (humanChoice === "rock" && computerChoice === 0) ||
-  (humanChoice === "paper" && computerChoice === 1) ||
-  (humanChoice === "scissors" && computerChoice === 2)
-) {
-  console.log("It is a tie!");
-} else if (
-  (humanChoice === "rock" && computerChoice === 2) ||
-  (humanChoice === "paper" && computerChoice === 0) ||
-  (humanChoice === "scissors" && computerChoice === 1)
-) {
-  console.log("Player won!");
-humanScore++;
-} else {
-  console.log("Computer won!");
-  computerScore++;
-}
+// Set up button listeners (OUTSIDE of playRound)
+const rockButton = document.querySelector("#rock");
+rockButton.addEventListener("click", () => {
+  humanChoice = "ROCK";
+  playRound();
+  roundCount++;
+  gameOver();
+});
 
+const paperButton = document.querySelector("#paper");
+paperButton.addEventListener("click", () => {
+  humanChoice = "PAPER";
+  playRound();
+  roundCount++;
+  gameOver();
+});
 
-}
-}
-playGame()
-
-if (humanScore > computerScore) {
-  console.log("PLAYER IS THE CHAMPION!");
-} else {
-  console.log("COMPUTER IS THE CHAMPION!");
-}
+const scissorsButton = document.querySelector("#scissors");
+scissorsButton.addEventListener("click", () => {
+  humanChoice = "SCISSORS";
+  playRound();
+  roundCount++;
+  gameOver();
+});
